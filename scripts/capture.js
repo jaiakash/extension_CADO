df62b9f6de2d0d8a6affff3ed71544115100f589
Akash Jaiswal <akashjaiswal3846@gmail.com>
2023-02-16 17:58:54 +0530

    feat: storage on click

--- scripts/capture.js
+++ scripts/capture.js
@@ -1,5 +1,15 @@
 console.log("capture.js loaded");
 document.onclick = (e) => {
+  // Change the colour of the clicked element
   e.target.style.backgroundColor = "red";
-  console.log(e.target.innerHTML);
+
+  // Get the text of the clicked element
+  const text = e.target.innerHTML;
+  console.log("Clicked on: ", text);
+
+  // Save the text to localStorage
+  localStorage.setItem("Text", text);
+
+  // Alert the user that the content has been saved
+  alert("Content has been saved!");
 };
