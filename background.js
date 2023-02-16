edbae36187757cfd0d932c0b01317d0b2bd50680
Akash Jaiswal <akashjaiswal3846@gmail.com>
2023-02-16 22:28:03 +0530

    feat: integrated chatgpt api

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-console.log("capture.js loaded");
+console.log("CADO Extention -> background.js loaded");
 
 chrome.runtime.onInstalled.addListener((tab) => {
   chrome.action.setBadgeText({
@@ -9,7 +9,6 @@ chrome.runtime.onInstalled.addListener((tab) => {
 chrome.action.onClicked.addListener(async (tab) => {
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
   const nextState = prevState === "ON" ? "OFF" : "ON";
-  console.log("nextState: " + nextState);
 
   // Set the action badge to the next state
   chrome.action.setBadgeText({
--- scripts/capture.js
+++ scripts/capture.js
@@ -1,13 +1,39 @@
+import "../secrets.json";
+
+function sendTextToOpenAI(text) {
+  // Post the text to OpenAI API
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0,
+      max_tokens: 2048,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      Authorization: "Bearer " + SECRET_KEY,
+    },
+  }).then((response) => {
+    response.json().then((data) => {
+      alert(data.choices[0].text);
+    });
+  });
+}
+
 document.onclick = (e) => {
   // Change the colour of the clicked element
   e.target.style.backgroundColor = "red";
 
   // Get the text of the clicked element
   const text = e.target.innerHTML;
-  console.log("Clicked on: ", text);
+  // console.log("Clicked on: ", text);
 
   // Save the text to localStorage
   localStorage.setItem("Text", text);
+
+  // Send the text to OpenAI
+  sendTextToOpenAI(text);
 
   // Alert the user that the content has been saved
   // alert("Content has been saved!");
